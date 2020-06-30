f66a9f29c170b5643b5c588b82a2071cd6a9cf17
alexandrepping <alexandrefeevale@gmail.com>
2020-06-29 22:58:09 -0300

    criado função para o botao editar mas sem salvar ainda

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let titleInput = document.getElementById("messageTitle");
+let editTitleInput = document.getElementById("editMessageTitle");
 let messageInput = document.getElementById("messageBody");
+let editMessageInput = document.getElementById("editMessageBody");
 let addButton = document.getElementById("addButton");
 let scrapsField = document.getElementById("scrapsField");
+let btnSaveEdit = document.getElementById("saveEdit");
 
 let scraps = [];
 
@@ -9,7 +12,12 @@ function renderScraps() {
   scrapsField.innerHTML = "";
 
   for (const scrap of scraps) {
-    scrapsField.innerHTML += createScrapCard(scrap.title, scrap.message);
+    let position = scraps.indexOf(scrap);
+    scrapsField.innerHTML += createScrapCard(
+      scrap.title,
+      scrap.message,
+      position
+    );
   }
 }
 
@@ -28,7 +36,7 @@ function addNewScrap() {
   renderScraps();
 }
 
-function createScrapCard(title, message) {
+function createScrapCard(title, message, position) {
   return `
     <div class="message-cards card text-white bg-dark m-2">
               <div class="card-header font-weight-bold">${title}</div>
@@ -41,8 +49,7 @@ function createScrapCard(title, message) {
                 <button class="btn btn-danger mr-1">Deletar</button>
                 <button
                   class="btn btn-info"
-                  data-toggle="modal"
-                  data-target="#editModal"
+                  onclick="openEditModal(${position})"
                 >
                   Editar
                 </button>
@@ -50,5 +57,16 @@ function createScrapCard(title, message) {
             </div>
   `;
 }
+
+function openEditModal(position) {
+  $("#editModal").modal("toggle");
+
+  editTitleInput.value = scraps[position].title;
+  editMessageInput.value = scraps[position].message;
+
+  btnSaveEdit.setAttribute("onclick", `savechanges(${position})`);
+}
+
+function saveChanges(position) {}
 
 addButton.onclick = addNewScrap;
