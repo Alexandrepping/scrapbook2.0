c955510f42f4cc8402da35528ac8bd7c40e5349c
alexandrepping <alexandrefeevale@gmail.com>
2020-06-29 21:06:46 -0300

    add scrapsField no JS , add style de scroll e renderização

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ let scrapsField = document.getElementById("scrapsField");
 
 let scraps = [];
 
+function renderScraps() {
+  scrapsField.innerHTML = "";
+
+  for (const scrap of scraps) {
+    scrapsField.innerHTML += createScrapCard(scrap.title, scrap.message);
+  }
+}
+
 function addNewScrap() {
   let title = titleInput.value;
   let message = messageInput.value;
@@ -13,26 +21,27 @@ function addNewScrap() {
     return `todos os campo precisam estar preechidos`;
   }
 
-  scrapsField.innerHTML = "";
   titleInput.value = "";
   messageInput.value = "";
   scraps.push({ title, message });
 
-  for (const scrap of scraps) {
-    scrapsField.innerHTML += createScrapCard(scrap.title, scrap.message);
-  }
+  renderScraps();
 }
 
 function createScrapCard(title, message) {
   return `
   <div class="message-cards card text-white bg-dark m-2">
-    <div class="card-header font-weight-bold">${title}</div>
-    <div class="card-body">
-      <p class="card-text">
-        ${message}
-      </p>
-    </div>
-  </div>
+              <div class="card-header font-weight-bold">${title}</div>
+              <div class="card-body">
+                <p class="card-text">
+                  ${message}
+                </p>
+              </div>
+              <div class="w100 d-flex justify-content-end pr-2 pb-2">
+                <button class="btn btn-danger mr-1">Deletar</button>
+                <button class="btn btn-info">Editar</button>
+              </div>
+            </div>
   `;
 }
 
