1ee2427ae19d4a36e730d08d0ad312b28650ca0e
alexandrepping <alexandrefeevale@gmail.com>
2020-07-13 20:12:29 -0300

    create delete scrap, create metodo setaddbuttonEvents, alterado name setAddbuttonEvents

--- bundle.js
+++ bundle.js
@@ -1,23 +1,64 @@
-class Tasklist {
+class TaskList {
   constructor() {
-    this.titleImput = document.getElementById("messageTitle");
-    this.messageimput = document.getElementById("messageBody");
-    this.addButton = document.getElementById("addutton");
-    this.scrapsfield = document.getElementById("scrapsField");
+    this.titleInput = document.getElementById("messageTitle");
+    this.messageInput = document.getElementById("messageBody");
+    this.addButton = document.getElementById("addButton");
+    this.scrapsField = document.getElementById("scrapsField");
     this.scraps = [];
-    this.registerEvennts();
+    this.setAddButtonEvents();
+  }
+
+  setAddButtonEvents() {
+    this.addButton.onclick = () => this.addNewScrap();
+  }
+
+  setButtonEvents() {
+    console.log(document.querySelectorAll(".delete-button"));
   }
 
   renderScraps() {
-    this.scrapsfield.innerHTML = "";
+    this.scrapsField.innerHTML = "";
 
     for (const scrap of this.scraps) {
-      this.scrapsfield.innerHTML += this.createScrapCard(scrap.title, scrap.message);
+      let position = scraps.indexOf(scrap);
+      this.scrapsField.innerHTML += this.createScrapCard(scrap.title, scrap.message, position);
     }
   }
 
-  createScrapCard(title, message) {}
+  addNewScrap() {
+    let title = this.titleInput.value;
+    let message = this.messageInput.value;
+    this.titleInput.value = "";
+    this.messageInput.value = "";
+    this.scraps.push({
+      title,
+      message
+    });
+    this.renderScraps();
+  }
+
+  deleteScrap(position) {
+    this.scraps.slice(position, 1);
+    this.renderScraps();
+  }
+
+  createScrapCard(title, message) {
+    return `
+      <div class="message-cards card text-white bg-dark m-2 col-3">
+        <div class="card-header font-weight-bold">${title}</div>
+        <div class="card-body">
+          <p class="card-text">
+            ${message}
+          </p>
+        </div>
+        <div class="w-100 d-flex justify-content-end pr-2 pb-2">
+          <button class="btn btn-danger mr-1 delete-button">Deletar</button>
+          <button class="btn btn-info">Editar</button>
+        </div>
+      </div>
+    `;
+  }
 
 }
 
-new Tasklist();
+new TaskList();
--- main.js
+++ main.js
@@ -7,20 +7,26 @@ class TaskList {
 
     this.scraps = [];
 
-    this.registerEvents();
+    this.setAddButtonEvents();
   }
 
-  registerEvents() {
-    this.addButton.onclick = this.addNewScrap();
+  setAddButtonEvents() {
+    this.addButton.onclick = () => this.addNewScrap();
+  }
+
+  setButtonEvents() {
+    console.log(document.querySelectorAll(".delete-button"));
   }
 
   renderScraps() {
     this.scrapsField.innerHTML = "";
 
     for (const scrap of this.scraps) {
+      let position = scraps.indexOf(scrap);
       this.scrapsField.innerHTML += this.createScrapCard(
         scrap.title,
-        scrap.message
+        scrap.message,
+        position
       );
     }
   }
@@ -37,6 +43,12 @@ class TaskList {
     this.renderScraps();
   }
 
+  deleteScrap(position) {
+    this.scraps.slice(position, 1);
+
+    this.renderScraps();
+  }
+
   createScrapCard(title, message) {
     return `
       <div class="message-cards card text-white bg-dark m-2 col-3">
@@ -47,7 +59,7 @@ class TaskList {
           </p>
         </div>
         <div class="w-100 d-flex justify-content-end pr-2 pb-2">
-          <button class="btn btn-danger mr-1">Deletar</button>
+          <button class="btn btn-danger mr-1 delete-button">Deletar</button>
           <button class="btn btn-info">Editar</button>
         </div>
       </div>
