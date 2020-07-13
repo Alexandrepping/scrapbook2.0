12a09596fe23123d871d756ecaccbca0497456ea
alexandrepping <alexandrefeevale@gmail.com>
2020-07-13 19:39:09 -0300

    add scrap incompleto

--- bundle.js
+++ bundle.js
@@ -1,1 +1,23 @@
-alert("teste");
+class Tasklist {
+  constructor() {
+    this.titleImput = document.getElementById("messageTitle");
+    this.messageimput = document.getElementById("messageBody");
+    this.addButton = document.getElementById("addutton");
+    this.scrapsfield = document.getElementById("scrapsField");
+    this.scraps = [];
+    this.registerEvennts();
+  }
+
+  renderScraps() {
+    this.scrapsfield.innerHTML = "";
+
+    for (const scrap of this.scraps) {
+      this.scrapsfield.innerHTML += this.createScrapCard(scrap.title, scrap.message);
+    }
+  }
+
+  createScrapCard(title, message) {}
+
+}
+
+new Tasklist();
